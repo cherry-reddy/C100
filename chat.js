63c8bfbf6877000c964c52f4afd0a354a0d56dbe
cherry-reddy <145208603+cherry-reddy@users.noreply.github.com>
2024-02-16 09:17:09 -0500

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,6 +1,14 @@
 // Your web app's Firebase configuration
 //ADD YOUR FIREBASE LINKS HERE
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBoQ18GVUBjsdFeM6SrRp6qvhTkgDo6Dv4",
+    authDomain: "chatwebapp-9959c.firebaseapp.com",
+    databaseURL: "https://chatwebapp-9959c-default-rtdb.firebaseio.com",
+    projectId: "chatwebapp-9959c",
+    storageBucket: "chatwebapp-9959c.appspot.com",
+    messagingSenderId: "808060325714",
+    appId: "1:808060325714:web:2a3e286b74f0c7491d748b"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -17,6 +25,9 @@ function addUser()
 
     window.location = "chat_room.html";
     */
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
 }
 
 
